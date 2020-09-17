1c1532c391b2a4a80ae3597712247175fd4ecdf5
unknown <18521447@gm.uit.edu.vn>
2020-09-17 04:02:14 -0700

    Update test_tool.py

--- test_tool.py
+++ test_tool.py
@@ -1,58 +1,125 @@
-import io
+import os
 import sys
 import subprocess
-from time import time
-from  subprocess import PIPE
-from subprocess import Popen
+from time import perf_counter
+
+def report(test_id, user_output, correct_output, user_runtime, time_limit, verbose=False):
+    '''
+        Print test results to screen
+    '''
+    report_text = []
+
+    MAX_LEN = 42
+
+    report_text.append('-' * MAX_LEN)
+    report_text.append('\n')
+
+    report_text.append('TEST {}: '.format(test_id))
+    len_so_far = len(report_text[-1])
+
+    if user_output == correct_output:
+        if time_limit != None and user_runtime > time_limit:
+            return_code = 'TLE'
+        else:
+            return_code = 'PASS'
+    else:        
+        return_code = 'FAILED'
+    
+    LEFT_MARGIN = 9
+    padding = MAX_LEN - len_so_far - LEFT_MARGIN
+
+    report_text.append(' ' * padding)
+    report_text.append(return_code)
+
+    len_so_far += padding
+    len_so_far += len(return_code)
+    padding = MAX_LEN - len_so_far - 2
+
+    report_text.append(' ' * padding)
+    
+    mark = '✅'
+    if return_code != 'PASS':
+        mark = '❌'
+        
+    report_text.append(mark)
+    report_text.append('\n')
+
+    if verbose:
+        if return_code != 'PASS':
+            report_text.append('Correct output\n')
+            report_text.append(correct_output)
+            report_text.append('\n')
+            
+            report_text.append('Your output\n')
+            report_text.append(process_output)
+            report_text.append('\n')
+
+            report_text.append('Correct output\'s length: ' + str(len(correct_output)))
+            report_text.append('\n')
+            report_text.append('Your    output\'s length: ' + str(len(process_output)))
+            report_text.append('\n')
+            
+        report_text.append('\n')
+        report_text.append('Ran in {:.5f} s'.format(process_runtime))
+        report_text.append('\n')
+
+    print(''.join(report_text), end='')
 
 if __name__ == '__main__':
 
     try: 
-        open(sys.argv[1])
+        f = open(sys.argv[1])
     except FileNotFoundError:
         print(sys.argv[1] + ' not found')
         raise
     finally:
-        python_file = sys.argv[1]
+        f.close()
+        
+    python_file = sys.argv[1]
+    
+    test_folder = sys.argv[2]
 
     try:
-        input_file = open(sys.argv[2])
-    except FileNotFoundError:
-        print(sys.argv[2] + ' not found')
-        raise
+        time_limit = float(sys.argv[3])
+    except IndexError:
+        time_limit = None
+    except ValueError:
+        time_limit = None
 
-    try:
-        output_file = open(sys.argv[3])
-    except FileNotFoundError:
-        print(sys.argv[3] + ' not found')
-        raise
-
-    inputs = input_file.read().split('---\n')
-    num_inputs = len(inputs)
-    correct_outputs = output_file.read().split('---\n')
+    is_verbose = False
+    if sys.argv[-1] == '-v':
+        is_verbose = True
+    
+    num_inputs = len(os.listdir(os.path.join(test_folder, 'inp')))
 
     command = ['python', python_file]
-
-    print('-' * 30)
-
-    for i in range(num_inputs):
-        start = time()
-        process_output = Popen(command, stdin=PIPE, stdout=PIPE, text=True).communicate(inputs[i])[0]
-        end = time()
+    
+    # Run tests
+    for test_id in range(1, num_inputs + 1):
+        inp = open(os.path.join(test_folder, 'inp', str(test_id)))
+        out = open(os.path.join(test_folder, 'out', str(test_id)))
         
-        print('TEST {}: '.format(i + 1), end='')
+        correct_output = out.read()
+        
+        start = perf_counter()
 
-        if process_output == correct_outputs[i]:
-            print('PASS      ✅')
-        else:
-            print('FAILED    ❌')
-            print('Correct output')
-            print(correct_outputs[i])
-            print('Your output')
-            print(process_output)
-            # print('Correct output\'s length:', len(correct_outputs[i]))
-            # print('Your    output\'s length:', len(process_output))
+        process_output = subprocess.run(
+            command, 
+            stdin=inp, 
+            text=True, 
+            capture_output=True
+        ).stdout
 
-        # print('Ran in', end - start, 's')
+        process_runtime = perf_counter() - start
 
-        print('-' * 30)
+        report(
+            test_id,
+            process_output,
+            correct_output, 
+            process_runtime, 
+            time_limit,
+            is_verbose
+        )
+
+        inp.close()
+        out.close()
